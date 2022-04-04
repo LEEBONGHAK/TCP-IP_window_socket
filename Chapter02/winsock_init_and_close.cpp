d2f4df649014ba4e5513a4b3ade942fbbce9ecbd
Bonghak Lee <bolee@c2r5s2.42seoul.kr>
2022-04-04 20:44:18 +0900

    winsock init and clean

---
+++ Chapter02/winsock_init_and_close.cpp
@@ -0,0 +1,15 @@
+#pragma comment(lib, "ws2_32")
+#include <winsock2.h>
+
+int main()
+{
+	// 윈속 초기화
+	WSADATA wsa;
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+		return 1;
+	MessageBox(NULL, "원속 초기화 성공", "알림", MB_OK);
+
+	// 윈속 종료
+	WSACleanup();
+	return 0;
+}
