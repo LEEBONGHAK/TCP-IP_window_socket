6530c9f427ae763e31b4ef53c7433f4bac3fd4a8
이봉학 <dlqhdgkr456@gmail.com>
2022-04-09 15:52:01 +0900

    Update TCPServer.cpp

--- Chapter04/IPv4_server_client/TCPServer.cpp
+++ Chapter04/IPv4_server_client/TCPServer.cpp
@@ -1,4 +1,5 @@
 #pragma comment(lib, "ws2_32")
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
 #include <winsock2.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -7,35 +8,35 @@
 #define BUFSIZE		512
 
 // 소켓 함수 오류 출력 후 종료
-void err_quit(char *msg)
+void err_quit(const char* msg)
 {
 	LPVOID lpMsgBuf;
 	FormatMessage(
-		FORMAT_MESSAGE_ALLOCATE_BUFFER|FORMAT_MESSAGE_FROM_SYSTEM,
-		NULL, MSAGetLastError(),
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, WSAGetLastError(),
 		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-		(LPTSTR) &lpMsgBuf, 0, NULL
+		(LPTSTR)&lpMsgBuf, 0, NULL
 	);
-	MessageBox(NULL, (LPCTSTR) lpMsgBuf, msg, MB_ICONERROR);
+	MessageBox(NULL, (LPCTSTR)lpMsgBuf, msg, MB_ICONERROR);
 	LocalFree(lpMsgBuf);
 	exit(1);
 }
 
 // 소켓 함수 오류 출력
-void err_display(char *msg)
+void err_display(const char* msg)
 {
 	LPVOID lpMsgBuf;
 	FormatMessage(
-		FORMAT_MESSAGE_ALLOCATE_BUFFER|FORMAT_MESSAGE_FROM_SYSTEM,
-		NULL, MSAGetLastError(),
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, WSAGetLastError(),
 		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-		(LPTSTR) &lpMsgBuf, 0, NULL
+		(LPTSTR)&lpMsgBuf, 0, NULL
 	);
-	printf("[%s] %s\n", msg, (char *) lpMsgBuf);
+	printf("[%s] %s\n", msg, (char*)lpMsgBuf);
 	LocalFree(lpMsgBuf);
 }
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
 	int retval;
 
@@ -55,10 +56,10 @@ int main(int argc, char *argv[])
 	serveraddr.sin_family = AF_INET;
 	serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
 	serveraddr.sin_port = htons(SERVERPORT);
-	retval = bind(listen_sock, (SOCKADDR *) &serveraddr, sizeof(serveraddr));
+	retval = bind(listen_sock, (SOCKADDR*)&serveraddr, sizeof(serveraddr));
 	if (retval == SOCKET_ERROR)
 		err_quit("bind()");
-	
+
 	// listen()
 	retval = listen(listen_sock, SOMAXCONN);
 	if (retval == SOCKET_ERROR)
@@ -68,13 +69,13 @@ int main(int argc, char *argv[])
 	SOCKET client_sock;
 	SOCKADDR_IN clientaddr;
 	int addrlen;
-	char bur[BUFSIZE + 1];
+	char buf[BUFSIZE + 1];
 
 	while (1)
 	{
 		// accept()
 		addrlen = sizeof(clientaddr);
-		client_sock = accept(listen_sock, (SOCKADDR *) &clientaddr, &addrlen);
+		client_sock = accept(listen_sock, (SOCKADDR*)&clientaddr, &addrlen);
 		if (client_sock == INVALID_SOCKET)
 		{
 			err_display("accept()");
@@ -96,7 +97,7 @@ int main(int argc, char *argv[])
 			}
 			else if (retval == 0)
 				break;
-			
+
 			// 받은 데이터 출력
 			buf[retval] = '\0';
 			printf("[TCP/%s:%d] %s\n", inet_ntoa(clientaddr.sin_addr), ntohs(clientaddr.sin_port), buf);
@@ -109,7 +110,7 @@ int main(int argc, char *argv[])
 				break;
 			}
 		}
-		
+
 		// closesocket()
 		closesocket(client_sock);
 		printf("[TCP 서버] 클라이언트 종료: IP 주소=%s, 포트 번호=%d\n", inet_ntoa(clientaddr.sin_addr), ntohs(clientaddr.sin_port));
