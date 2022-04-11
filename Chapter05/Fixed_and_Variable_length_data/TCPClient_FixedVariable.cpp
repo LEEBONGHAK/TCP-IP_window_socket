d91ce4823a1855933373a78c15dc1757d567fe3c
Bonghak Lee <bolee@c2r6s5.42seoul.kr>
2022-04-11 18:16:56 +0900

    Fixed and Variable length data

--- Chapter05/Fixed_and_Variable_length_data/TCPClient_FixedVariable.cpp
+++ Chapter05/Fixed_and_Variable_length_data/TCPClient_FixedVariable.cpp
@@ -36,7 +36,7 @@ void err_display(char *msg)
 	LocalFree(lpMsgBuf);
 }
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
 	int retval;
 
@@ -50,43 +50,54 @@ int main(int argc, char *argv[])
 	if (sock == INVALID_SOCKET)
 		err_quit("socket()");
 
-	// connect()
+	// connet()
 	SOCKADDR_IN serveraddr;
 	ZeroMemory(&serveraddr, sizeof(serveraddr));
 	serveraddr.sin_family = AF_INET;
-	serveraddr.sin_addr.s_addr = inet_addr(SERVERIP);
+	serveraddr.sin_addr.s_addr = inet_ntoa(SERVERIP);
 	serveraddr.sin_port = htons(SERVERPORT);
-	retval = connect(sock, (SOCKADDR *) &serveraddr, sizeof(serveraddr));
+	retval = connet(sock, (SOCKADDR *) &serveraddr, sizeof(serveraddr));
 	if (retval == SOCKET_ERROR)
-		err_quit("connect()");
+		err_quit("connet()");
 
-	// 데이터 동신에 사용할 변수
-	char buf[BUFSIZE];
-	char *testdata[] = {
+	// 데이터 통신에 사용할 변수
+	char buf[BUFSIZE];	// BUFSIZE 크기인 버퍼 선언
+	char *testdata[] = {	// 전송할 문자열 데이터 선언
 		"안녕하세요",
 		"반가워요",
 		"오늘따라 할 이야기가 많을 것 같네요",
 		"저도 그렇네요",
 	};
+	int len;	// 문자열 길이를 계산한 결과를 받는다.
 
-	// 서버와 데이터 통신
+	// 서버과 데이터 통신
 	for (int i = 0; i < 4; i++)
 	{
 		// 데이터 입력(시뮬레이션)
-		memset(buf, "#", sizeof(buf));
-		strncpy(buf, testdata[i], strlen(testdata[i]));
+		len = strlen(testdata[i]);
+		strncpy(buf, testdata[i], len);	// 문자열 데이터를 버퍼에 복사
 
-		// 데이터 보내기
-		retval = send(sock, buf, BUFSIZE, 0);
+		// 데이터 보내기(고정 길이)
+		// 문자열 길이는 32비트(int) 고정 길이로 보낸다.
+		retval = send(sock, (char *) &len, sizeof(int), 0);
 		if (retval == SOCKET_ERROR)
 		{
 			err_display("send()");
 			break;
 		}
 
-		printf("[TCP 클라이언트] %d바이트를 보냈습니다.\n", retval);
-	}
+		// 데이터 보내기(가변 길이)
+		// 문자열 데이터를 가변 길이로 보낸다.
+		retval = send(sock, buf, len, 0);
+		if (retval == SOCKET_ERROR)
+		{
+			err_display("send()");
+			break;
+		}
 
+		printf("[TCP 클라이언트] %d+%d바이트를 보냈습니다.\n", sizeof(int), retval);
+	}
+	
 	// closesocket()
 	closesocket(sock);
 
---
+++ Chapter05/Fixed_and_Variable_length_data/TCPServer_FixedVariable.cpp
@@ -0,0 +1,149 @@
+#pragma comment(lib, "ws2_32")
+#include <winsock2.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define SERVERPORT	9000
+#define BUFSIZE		512	// 실제로 받을 데이터는 이보다 작다고 가정
+
+// 소켓 함수 오류 출력 후 종료
+void err_quit(char *msg)
+{
+	LPVOID lpMsgBuf;
+	FormatMessage(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER|FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, WSAGetLastError(),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPTSTR) &lpMsgBuf, 0, NULL
+	);
+	MessageBox(NULL, (LPCTSTR) lpMsgBuf, msg, MB_ICONERROR);
+	LocalFree(lpMsgBuf);
+	exit(1);
+}
+
+void err_display(char *msg)
+{
+	LPVOID lpMsgBuf;
+	FormatMessage(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER|FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, WSAGetLastError(),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPTSTR) &lpMsgBuf, 0, NULL
+	);
+	printf("[%s] %s\n", msg, (char *) lpMsgBuf);
+	LocalFree(lpMsgBuf);
+}
+
+// 사용자 정의 데이터 수신 함수
+// 고정 길이 데이터와 가변 길이 데이터를 읽는 데 사용
+int recvn(SOCKET s, char *buf, int len, int flags)
+{
+	int received;
+	char *ptr = buf;
+	int left = len;
+
+	while (left > 0)
+	{
+		received = recv(s, ptr, left, flags);
+		if (received == SOCKET_ERROR)
+			return SOCKET_ERROR;
+		else if (received == 0)
+			break;
+
+		left -= received;
+		ptr += received;
+	}
+
+	return (len - left);
+}
+
+int main(int argc, char **argv)
+{
+	int retval;
+
+	// 윈속 초기화
+	WSADATA wsa;
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+		return 1;
+
+	// socket()
+	SOCKET listen_socke = socket(AF_INET, SOCK_STREAM, 0);
+	if (listen_sock == INVALID_SOCKET)
+		err_quit("socket()");
+
+	// bind()
+	SOCKADDR_IN serveraddr;
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serveraddr.sin_port = htons(SERVERPORT);
+	retval = bind(listen_sock, (SOCKADDR *) &serveraddr, sizeof(serveraddr));
+	if (retval == SOCKET_ERROR)
+		err_quit("bind()");
+
+	// listen()
+	retval = listen(listen_sock, SOMAXCONN);
+	if (retval == SOCKET_ERROR)
+		err_quit("listen()");
+
+	// 데이터 통신에 사용할 변수
+	SOCKET client_sock;
+	SOCKADDR_IN clientaddr;
+	int addrlen;
+	char buf[BUFSIZE + 1];	// 1바이트 null 문자를 감안해 선언
+	int len;	// 32비트(int) 고정 길이 데이터를 받을 때 사용
+
+	while (1)
+	{
+		// accept()
+		client_sock = accept(listen_sock, (SOCKADDR *) &clientaddr, sizeof(clientaddr));
+		if (client_sock == INVALID_SOCKET)
+		{
+			err_display("accept()");
+			break;
+		}
+
+		// 접속한 클라이언트 정보 출력
+		printf("\n[TCP 서버] 클라이언트 접속: IP 주소=%s, 포트 번호=%d\n", inet_ntoa(clientaddr.sin_addr), ntohs(clientaddr.sin_port));
+
+		// 클라이언트와 데이터 통신
+		while (1)
+		{
+			// 데이터 받기(고정길이)
+			// 32비트(int) 고정 길이 데이터를 읽는다. len 변수에는 뒤따르는 가변 길이 데이터의 크기가 저장된다.
+			retval = recvn(client_sock, (char *) &len, sizeof(int), 0);
+			if (retval == SOCKET_ERROR)
+			{
+				err_display("recv()");
+				break;
+			}
+			else if (retval == 0)
+				break;
+
+			// 데이터 받기(가변 길이)
+			// len 길이인 데이터를 한꺼번에 읽는다.
+			retval = recv(client_sock, buf, len, 0);
+			if (retval == SOCKET_ERROR)
+			{
+				err_display("recv()");
+				break;
+			}
+			else if (retval == 0)
+				break;
+
+			// 받은 데이터 출력
+			buf[retval] = '\0';
+			printf("[TCP/%s:%d] %s\n", inet_ntoa(clientaddr.sin_addr), ntohs(clientaddr.sin_port), buf);
+		}
+
+		// closesocket()
+		closesocket(client_sock);
+		printf("[TCP 서버] 클라이언트 종료: IP 주소=%s, 포트 번호=%d\n", inet_ntoa(clientaddr.sin_addr), ntohs(clientaddr.sin_port));
+	}
+
+	// closesocket()
+	closesocket(listen_sock);
+
+	// 윈속 종료
+	WSACleanup();
+	return 0;
+}
