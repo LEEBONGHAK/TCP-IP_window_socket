d7a895cb6384c42a05d5d0faaca4a2741cadaf5c
Bonghak Lee <bolee@c2r6s5.42seoul.kr>
2022-04-11 19:37:43 +0900

    Close on transfer

---
+++ Chapter05/Close_on_transfer/TCPClient_CloseOnTransfer.cpp
@@ -0,0 +1,103 @@
+#pragma comment(lib,"ws2_32")
+#include <winsock2.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define SERVERIP	"127.0.0.1"
+#define SERVERPORT	9000
+#define BUFSIZE		50	// 50바이트 버퍼를 사용해 데이터를 보낸다.
+
+// 소켓 함수 오류 출력 후 종료
+void err_quit(char *msg)
+{
+	LPVOID lpMsgBuf;
+	FormatMessage(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER|FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, WSAGetLastError(),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPTSTR) &lpMsgBuf, 0, NULL
+	);
+	MessageBox(NULL, (LPCTSTR) lpMsgBuf, msg, MB_ICONERROR);
+	LocalFree(lpMsgBuf);
+	exit(1);
+}
+
+// 소켓 함수 오류 출력
+void err_dispaly(char *msg)
+{
+	LPVOID lpMsgBuf;
+	FormatMessage(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER|FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, WSAGetLastError(),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPTSTR) &lpMsgBuf, 0, NULL
+	);
+	printf("[%s] %s\n", msg, (char *) lpMsgBuf);
+	LocalFree(lpMsgBuf);
+}
+
+int main(int argc, char **argv)
+{
+	int retval;
+
+	// 윈속 초기화
+	WSADATA wsa;
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+		return 1;
+
+	// connect() 호출에 사용할 변수
+	SOCKADDR_IN serveraddr;
+	ZeroMemory(&serveraddr, sizeof(serveraddr));
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_addr.s_addr = inet_ntoa(SERVERIP);
+	serveraddr.sin_port = htons(SERVERPORT);
+	
+	// 데이터 통신에 사용할 변수
+	char buf[BUFSIZE];	// BUFSIZE 크기인 버퍼를 선언
+	char *testdata[] = {	// 전송할 문자열 데이터 선언
+		"안녕하세요",
+		"반가워요",
+		"오늘따라 할 이야기가 많을 것 같네요",
+		"저도 그렇네요",
+	};
+	int len;	// 문자열 길이를 담을 변수 선언
+
+	// 서버와 통신
+	for (int i = 0; i < 4; i++)
+	{
+		// socket()
+		// 데이터를 보낼 때마다 소켓 생성
+		SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+		if (sock == INVALID_SOCKET)
+			err_quit("socket()");
+
+		// connect()
+		// 데이터를 보낼 때마다 서버 접속
+		retval = connect(sock, (SOCKADDR *) &serveraddr, sizeof(serveraddr));
+		if (retval == SOCKET_ERROR)
+			err_quit("connect()");
+
+		// 데이터 입력(시뮬레이션)
+		len = strlen(testdata[i]);
+		strncpy(buf, testdata[i], len);	// 문자열 데이터를 버퍼에 복사
+
+		// 데이터 보내기
+		// 문자열 데이터를 가변 길이로 보낸다.
+		retval = recvn(sock, buf, len, 0);
+		if (retval == SOCKET_ERROR)
+		{
+			err_dispaly("send()")
+			break;
+		}
+
+		printf("[TCP 클라이언트] %d바이트를 보냈습니다.\n", retval);
+
+		// closesocket()
+		// 데이터를 보낼 때마다 접속 종료
+		closesocket(sock);
+	}
+
+	// 윈속 종료
+	WSACleanup();
+	return 0;
+}
---
+++ Chapter05/Close_on_transfer/TCPServer_CloseOnTransfer.cpp
@@ -0,0 +1,138 @@
+#pragma comment(lib, "ws2_32")
+#include <winsock2.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define SERVERPORT	9000
+#define BUFSIZE		1024	// 실제로 받을 데이터는 이보다 작은 가변 길이 데이터라고 가정한다.
+
+// 소켓 함수 오류 출력 후 종료
+void err_quit(char *msg)
+{
+	LPVOID lpMsgBuf;
+	FormatMessage(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER|FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, WSAGetLastError(),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPTSTR) &lpMsgBuf, 0, NULL
+	);
+	MessageBox(NULL, (LPCTSTR) lpMsgBuf, msg, MB_ICONERROR);
+	LocalFree(lpMsgBuf);
+	exit(1);
+}
+
+// 소켓 함수 오류 출력
+void err_dispaly(char *msg)
+{
+	LPVOID lpMsgBuf;
+	FormatMessage(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER|FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, WSAGetLastError(),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPTSTR) &lpMsgBuf, 0, NULL
+	);
+	printf("[%s] %s\n", msg, (char *) lpMsgBuf);
+	LocalFree(lpMsgBuf);
+}
+
+// 사용자 정의 데이엍 수신 함수
+int recvn(SOCKET s, char *buf, int len, int flags)
+{
+	int received;
+	char *ptr = buf;
+	int left = len;
+
+	while (left > 0)
+	{
+		received = recv(s, ptr, left, flags);
+		if (received == SOCKET_ERROR)
+			return SOCKET_ERROR;
+		else if (received == 0)
+			break;
+		
+		left -= received;
+		ptr += received;
+	}
+
+	return (len - left);
+}
+
+int main(int argc, char **argv)
+{
+	int retval;
+
+	// 윈속 초기화
+	WSADATA wsa;
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+		return 1;
+
+	// socket()
+	SOCKET listen_sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (listen_sock == INVALID_SOCKET)
+		err_quit("socket()");
+	
+	// bind()
+	SOCKADDR_IN serveraddr;
+	ZeroMemory(&serveraddr, sizeof(serveraddr));
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_addr = htonl(INADDR_ANY);
+	serveraddr.sin_port = htons(SERVERPORT);
+	retval = bind(listen_sock, (SOCKADDR *) &serveraddr, sizeof(serveraddr));
+	if (retval == SOCKET_ERROR)
+		err_quit("bind()");
+	
+	// listen()
+	retval = listen(listen_sock, SOMAXCONN);
+	if (retval == SOCKET_ERROR)
+		err_quit("listen()");
+
+	// 데이터 통신에 사용할 변수
+	SOCKET client_sock;
+	SOCKADDR_IN clientaddr;
+	int addrlen;
+	char buf[BUFSIZE + 1];	// 1바이트 null 문자를 고려하여 버퍼 선언
+
+	while (1)
+	{
+		// accept()
+		addrlen = sizeof(clientaddr);
+		client_sock = accept(listen_sock, (SOCKADDR *) &clientaddr, &addrlen);
+		if (client_sock == INVALID_SOCKET)
+		{
+			err_dispaly("accept()");
+			break;
+		}
+
+		// 접속한 클라이언트 정보 출력
+		printf("\n[TCP 서버] 클라이언트 접속: IP 주소=%s, 포트 번호=%d\n", inet_ntoa(clientaddr.sin_addr), ntohs(clientaddr.sin_port));
+
+		while (1)
+		{
+			// 데이터 받기
+			// recvn() 함수를 호출해 데이터를 최대한 읽는다. 최대 길이를 BUFSIZE로 지정했지만 클라이언트가 BUFSIZE보다 작은 데이터를 전송한 후 접속을 종료한다고 가정한다.
+			retval = recvn(client_sock, buf, BUFSIZE, 0);
+			if (retval == SOCKET_ERROR)
+			{
+				err_dispaly("recv()");
+				break;
+			}
+			else if (retval == 0)
+				break;
+
+			// 받은 데이터 출력
+			buf[retval] = '\0';
+			printf("[TCP/%s:%d] %s\n", inet_ntoa(clientaddr.sin_addr), ntohs(clientaddr.sin_port), buf);
+		}
+
+		// closesocket()
+		closesocket(client_sock);
+		printf("[TCP/%s:%d] %s\n", inet_ntoa(clientaddr.sin_addr), ntohs(clientaddr.sin_port), buf);
+	}
+
+	// closesocket()
+	closesocket(listen_sock);
+
+	// 윈속 종료
+	WSACleanup();
+	return 0;
+}
