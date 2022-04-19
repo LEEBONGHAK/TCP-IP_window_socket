cbefc5af6ee1b2d87e45a7afcda6a66e767c4271
Bonghak Lee <bolee@c2r6s2.42seoul.kr>
2022-04-19 12:53:35 +0900

    thread

---
+++ Chapter06/ExThread2.cpp
@@ -0,0 +1,38 @@
+// 스레드 우선순위 변경 연습예제
+
+#include <windows.h>
+#include <stdio.h>
+
+DWORD WINAPI MyThread(LPVOID arg)
+{
+	while (1);
+	return 0;
+}
+
+int main()
+{
+	// CPU 개수를 알아낸다.
+	SYSTEM_INFO si;
+	GetSystemInfo(&si);
+
+	// CPU 개수만큼 스레드를 생성한다.
+	for (int i = 0; i < (int) si.dwNumberOfProcessors; i++)
+	{
+		HANDLE hThread = CreateThread(NULL, 0, MyThread, NULL, 0, NULL);
+		if (hThread == NULL)
+			return 1;
+
+		// 최고 순위로 변경
+		SetThreadPriority(hThread, THREAD_PRIORITY_CRITICAL);
+		CloseHandle(hThread);
+	}
+
+	Sleep(1000);
+	while (1)
+	{
+		printf("주 스레드 실행!\n");
+		break;
+	}
+
+	return 0;
+}
---
+++ Chapter06/ExThread3.cpp
@@ -0,0 +1,35 @@
+// 스레드 실행 제어와 종료 기다릭기 연습예제
+
+#include <windows.h>
+#include <stdio.h>
+
+int sum = 0;
+
+DWORD WINAPI MyThread(LPVOID arg)
+{
+	int num = (int) arg;
+
+	for (int i = 0; i < num; i++)
+		sum += i;
+
+	return 0;
+}
+
+int main(int argc, char **argv)
+{
+	int num = 100;
+
+	HANLE hThread = CreateThread(NULL, 0, MyThread, &num, CREATE_SUSPENDED, NULL);
+	if (hThread == NULL)
+		return 1;
+
+	printf("스레드 실행 전, 계산 결과 = $d\n", sum);
+
+	ResumeThread(hThread);
+	WaitForSingleObject(hThread, INFINITE);
+	
+	printf("스레드 실행 후, 계산 결과 = %d\n", sum);
+	CloseHandle(hThread);
+
+	return 0;
+}
