736535afd0347955f603406aa5428a632b80f174
Bonghak Lee <bolee@c2r6s2.42seoul.kr>
2022-05-02 14:51:52 +0900

    nonblocking socket

---
+++ Chapter10/NonblockingTCPServer/NonblockTCPServer.cpp
@@ -0,0 +1,137 @@
+#pragma comment(lib, "ws2_32")
+#include <winsock2.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define SERVERPORT	9000
+#define BUFSIZE		512
+
+// 오류 출력 후 종료
+void err_quit(const char *msg)
+{
+	LPVOID lpMsgBuf;
+	FormatMessage(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER|FROMAT_MESSAGE_FROM_SYSTEM,
+		NULL, WSAGetLastError(),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPTSTR) &lpMsgBuf, 0, NULL
+	);
+	MessageBox(NULL, (LPCTSTR) lpMsgBuf, msg, MB_ICONERROR);
+	LocalFree(lpMsgBuf);
+	exit(1);
+}
+
+// 오류 출력
+void err_display(const char *msg)
+{
+	LPVOID lpMsgBuf;
+	FormatMessage(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER|FROMAT_MESSAGE_FROM_SYSTEM,
+		NULL, WSAGetLastError(),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPTSTR) &lpMsgBuf, 0, NULL
+	);
+	printf("[%s] %s", msg, (char *) lpMsgBuf);
+	LocalFree(lpMsgBuf);
+}
+
+int main(int argc, char **argv)
+{
+	int retval;
+
+	// 윈속 초기화
+	WSADATA wsa;
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+		return 1;
+
+	// socket()
+	SOCKET listen_sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (listen_sock == INVALID_SOCKET)
+		err_quit("socket()");
+
+	// 넌블로킹 소켓으로 전환
+	u_long on = 1;
+	retval = ioctlsocket(listen_sock, FIONBIO, &on);
+	if (retval == SOCKET_ERROR)
+		err_quit("ioctlsocket()");
+
+	// bind()
+	SOCKADDR_IN serveraddr;
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serveraddr.sin_port = htons(SERVERPORT);
+	retval = bind(listen_sock, (SOCKADDR *) &serveraddr, sizeof(serveraddr));
+	if (retval == SOCKET_ERROR)
+		err_quit("bind()");
+
+	// listen()
+	retval = listen(listen_sock, SOMAXCONN);
+	if (retval == SOCKET_ERROR)
+		err_quit("listen()");
+
+	// 데이터 통신에 사용할 변수
+	SOCKET client_sock;
+	SOCKETADDR_IN clientaddr;
+	int addrlen;
+	char buf[BUFSIZE + 1];
+
+	while (1)
+	{
+		// accept()
+ACCEPT_AGAIN:
+		addrlen = sizeof(clientaddr);
+		// listen_sock 이 넌블로킹 소켓이면 자동으로 넌블로킹 소켓이 된다.
+		client_sock = accept(listen_sock, (SOCKADDR *) &clientaddr, &addrlen);
+		if (client_sock == INVALID_SOCKET)
+		{
+			if (WSAGetLastError() == WSAEWOULDBLOCK)
+				goto ACCEPT_AGAIN;
+			err_display("accept()");
+			break;
+		}
+
+		// 접속한 클라이언트 정보 출력
+		printf("\n[TCP 서버] 클라이언트 접속: IP 주소=%s, 포트 번호=%d\n", inet_ntoa(clientaddr.sin_addr), htons(clientaddr.sin_port));
+
+		// 클라이언트와 데이터 통신
+		while (1)
+		{
+RECEIVE_AGAIN:
+			retval = recv(client_sock, buf, BUFSIZE, 0);
+			if (retval == SOCKET_ERROR)
+			{
+				if (WSAGetLastError() == WSAEWOULDBLOCK)
+					goto RECEIVE_AGAIN;
+				err_display("recv()");
+				break;
+			}
+			else if (retval == 0)
+				break;
+
+			// 받은 데이터 출력
+			buf[retval] = '\0';
+			printf("[TCP/%s: %d] %s\n", inet_ntoa(clientaddr.sin_addr), ntohs(clientaddr.sin_port), buf);
+
+			// 데이터 보내기
+SEND_AGAIN:
+			retval = send(client_sock, buf, retval, 0);
+			if (retval == SOCKET_ERROR)
+			{
+				if (WSAGetLastError() == WSAEWOULDBLOCK)
+					goto SEND_AGAIN;
+				err_display("recv()");
+				break;
+			}
+		}
+
+		// closesocket()
+		closesocket(client_sock);
+	}
+
+	// closesocket()
+	closesocket(listen_sock);
+
+	// 윈속 종료
+	WSACleanup();
+	return 0;
+}
